b0db155f05892a25bc097bdf2fd4704e9bf25f1b
joshg <30822255+joshavenue@users.noreply.github.com>
2017-10-10 13:27:25 +0800

    Create class_4.py

---
+++ class_4.py
@@ -0,0 +1,16 @@
+# Add the extract_header() code to the initializer and set the header data to self.header.
+# Create a variable called nfl_header and set it to the header attribute
+
+class Dataset:
+    def __init__(self, data):
+        self.data = data
+
+nfl_dataset = Dataset(nfl_data)
+
+class Dataset:
+    def __init__(self,data):        # Somehow extract_header() becomes __init__()
+        self.header = data[0]
+        self.data = data[1:]
+        
+nfl_dataset = Dataset(nfl_data)
+nfl_header = nfl_dataset.header
